ad026489d73ea1ebbcdc3fd62a3a60a07991980c
Wayne Cho <waynecho2004@gmail.com>
2020-10-27 20:14:34 -0400

    create router for web

---
+++ controllers/webController.js
@@ -0,0 +1,9 @@
+// Include the express router
+const router = require('express').Router();
+// Include the schemas models
+const Web = require('../models/web').Web;
+const Url = require('../models/web').Url
+
+// TODO: create routes
+
+module.exports = router;
